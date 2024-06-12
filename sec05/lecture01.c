e9ed0da74b00666beca9ee5887bc45e865ccb0fa
sujin kim <mh030128@naver.com>
2024-06-12 15:33:57 +0900

    loopEx

--- sec05/lecture01.c
+++ sec05/lecture01.c
@@ -2,9 +2,17 @@
 
 int main()
 {
-	// Loop
+	// Loop (반복문)
 
-	printf("test");
+	int n = 1;
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n += 1;	// 입력하지 않으면 1만 출력하는 무한 Loop에 출력
+	}
+
+	// Debug 실행하면서 n 값이 어떻게 변하는지 확인하며 공부하는 것이 좋음
 
 	return 0;
 }
